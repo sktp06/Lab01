75ef5ed0258f15d7ffc8862e22c5b974a57310e1
sktp06 <kantaporn_s@cmu.ac.th>
2022-06-30 21:23:32 +0700

    checkpoint5

--- main.js
+++ main.js
@@ -11,6 +11,12 @@ const app = Vue.createApp({
             image: './assets/images/socks_green.jpg',
             inStock: true,
             inventory: 100,
+            details: ['50% cotton','30% wool', '20% polyester'],
+            varaints: [
+                { id: 2234, color: 'green' },
+                { id: 2235, color: 'blue' }
+            ],
+            sizes:['S','M','L'],
             onSale: true
         }
     }
