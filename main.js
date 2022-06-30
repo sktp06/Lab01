9c65bfd1ee988ba58fc22c7c0c495c05fec4f7b2
sktp06 <kantaporn_s@cmu.ac.th>
2022-06-30 10:50:49 +0700

    checkpoint2

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 const product = 'Socks'
+const app = Vue.createApp({
+    data() {
+        return {
+            product: 'Boots',
+            description: 'A boot is a type of footwear. Some boots extend up the leg, sometimes as far as the knee or even the hip.'
+        }
+    }
+})
