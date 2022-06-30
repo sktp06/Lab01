fdacb4069ef42e22868044c40af7c5a440216ece
sktp06 <kantaporn_s@cmu.ac.th>
2022-06-30 20:12:48 +0700

    checkpoint3

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const app = Vue.createApp({
     data() {
         return {
             product: 'Boots',
-            description: 'A boot is a type of footwear. Some boots extend up the leg, sometimes as far as the knee or even the hip.'
+            description: 'A boot is a type of footwear. Some boots extend up the leg, sometimes as far as the knee or even the hip.',
+            image: './assets/images/socks_green.jpg',
+            link: 'http://www.camt.cmu.ac.th'
         }
     }
 })
